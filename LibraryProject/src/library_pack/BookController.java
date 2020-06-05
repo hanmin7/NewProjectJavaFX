c889bb66b940d0ef382a3c58e88232c22520b449
admin <admin@YD03-01>
2020-06-05 17:40:47 +0900

    library_pack

--- LibraryProject/src/library_pack/BookController.java
+++ LibraryProject/src/library_pack/BookController.java
@@ -1,15 +1,28 @@
 package library_pack;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
 
 public class BookController implements Initializable {
-
+	Connection conn;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, "hr", "hr");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
+
+
+
+
--- LibraryProject/src/library_pack/BookMain.java
+++ LibraryProject/src/library_pack/BookMain.java
@@ -1,13 +1,22 @@
 package library_pack;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class BookMain extends Application{
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Root.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
