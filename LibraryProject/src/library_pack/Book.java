4969235b13f04234cc25b8cbc10ab8097ef913e5
admin <admin@YD03-01>
2020-06-05 16:47:50 +0900

    library_pack

---
+++ LibraryProject/src/library_pack/Book.java
@@ -0,0 +1,5 @@
+package library_pack;
+
+public class Book {
+
+}
---
+++ LibraryProject/src/library_pack/BookMain.java
@@ -0,0 +1,13 @@
+package library_pack;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BookMain extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}
+
+}
