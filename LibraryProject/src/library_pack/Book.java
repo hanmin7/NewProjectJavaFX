a59cd1e1abbcaab959c9b5788a9f597523d06f39
LGPC <LGPC@DESKTOP-G15CLQ0>
2020-06-07 15:11:25 +0900

    0607_library_test

--- LibraryProject/src/library_pack/Book.java
+++ LibraryProject/src/library_pack/Book.java
@@ -1,5 +1,72 @@
 package library_pack;
 
-public class Book {
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 
+public class Book {
+	private SimpleIntegerProperty id;
+	private SimpleStringProperty title;
+	private SimpleStringProperty writer;
+	private SimpleIntegerProperty status;
+	private SimpleStringProperty content;
+	
+	public Book (int id, String title, String writer, int status, String content) {
+		super();
+		this.id = new SimpleIntegerProperty(id);
+		this.title = new SimpleStringProperty(title);
+		this.writer = new SimpleStringProperty(writer);
+		this.status = new SimpleIntegerProperty(status);
+		this.content = new SimpleStringProperty(content);
+	}
+	
+	public void setId(Integer id) {
+		this.id.set(id);
+	}
+	public int getId() {
+		return this.id.get();
+	}
+	public SimpleIntegerProperty idProperty() {
+		return this.id;
+	}
+	
+	public void setTitle(String title) {
+		this.title.set(title);
+	}
+	public String getTitle() {
+		return this.title.get();
+	}
+	public SimpleStringProperty titleProperty() {
+		return this.title;
+	}
+	
+	public void setWriter(String writer) {
+		this.writer.set(writer);
+	}
+	public String getWriter() {
+		return this.writer.get();
+	}
+	public SimpleStringProperty writerProperty() {
+		return this.writer;
+	}
+	
+	public void setStatus(Integer status) {
+		this.status.set(status);
+	}
+	public int getStatus() {
+		return this.status.get();
+	}
+	public SimpleIntegerProperty statusProperty() {
+		return this.status;
+	}
+	
+	public void setContent(String content) {
+		this.content.set(content);
+	}
+	public String getContent() {
+		return this.content.get();
+	}
+	public SimpleStringProperty contentProperty() {
+		return this.content;
+	}
+	
 }
--- LibraryProject/src/library_pack/BookController.java
+++ LibraryProject/src/library_pack/BookController.java
@@ -3,13 +3,28 @@ package library_pack;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class BookController implements Initializable {
 	Connection conn;
+	
+	@FXML TableView<Book> tableView;
+	@FXML TableColumn<Book, Integer> id;
+	@FXML TableColumn<Book, String> title;
+	@FXML TableColumn<Book, String> wr;
+	@FXML TableColumn<Book, Integer> status;
+	@FXML TableColumn<Book, String> content;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -17,10 +32,46 @@ public class BookController implements Initializable {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection(url, "hr", "hr");
-		} catch (ClassNotFoundException | SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
+		ObservableList<Book> bookList = getBookList();
+		tableView.setItems(bookList);
+		
+//		TableColumn<Book, String> tcTitle = new TableColumn<Book, String>();
+//		tcTitle.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
+//		tcTitle.setText("책제목");
+//		tableView.getColumns().add(tcTitle);
+//		tableView.setItems(bookList);
+		
+		
+		id.setCellValueFactory(new PropertyValueFactory<Book, Integer>("id"));
+		title.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
+		wr.setCellValueFactory(new PropertyValueFactory<Book, String>("writer"));
+		status.setCellValueFactory(new PropertyValueFactory<Book, Integer>("status"));
+		
+		
+		
+		
+		
+	}//initialize
+	
+	
+	public ObservableList<Book> getBookList() {
+		ObservableList<Book> list = FXCollections.observableArrayList();
+		String sql = "select id, title, writer, status, content from book";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Book board = new Book(rs.getInt("id"), rs.getString("title"), rs.getString("writer"), rs.getInt("status"), rs.getString("content"));
+				list.add(board);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	} //getBookList
 }
 
 
