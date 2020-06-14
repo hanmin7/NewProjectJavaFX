c9d7abd70247a6ad788bb89e3d392a87c6f5cd07
admin <admin@YD03-01>
2020-06-15 08:52:08 +0900

    0610

--- LibraryProject/src/library_pack/BookController.java
+++ LibraryProject/src/library_pack/BookController.java
@@ -29,24 +29,33 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class BookController implements Initializable {
 	Connection conn;
-	
-	@FXML TableView<Book> tableView;
-	@FXML TableColumn<Book, Integer> id;
-	@FXML TableColumn<Book, String> title;
-	@FXML TableColumn<Book, String> wr;
-	@FXML TableColumn<Book, Integer> status;
-	@FXML TableColumn<Book, String> content;
-	@FXML TableColumn<Book, String> image;
-	@FXML Button btnList;
-	
+
+	@FXML
+	TableView<Book> tableView;
+	@FXML
+	TableColumn<Book, Integer> id;
+	@FXML
+	TableColumn<Book, String> title;
+	@FXML
+	TableColumn<Book, String> wr;
+	@FXML
+	TableColumn<Book, Integer> status;
+	@FXML
+	TableColumn<Book, String> content;
+	@FXML
+	TableColumn<Book, String> image;
+	@FXML
+	Button btnList;
+
 	Book book;
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
@@ -74,6 +83,7 @@ public class BookController implements Initializable {
 			}
 		});
 		
+		
 		btnList.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
@@ -81,59 +91,77 @@ public class BookController implements Initializable {
 			}
 		});
 		
+//		tableView.setOnMouseClicked((new EventHandler<MouseEvent>() {
+//
+//			@Override
+//			public void handle(MouseEvent event) {
+//				if (event.getClickCount() == 2) {
+//					
+//					Stage addStage = new Stage(StageStyle.UTILITY);
+//					addStage.initModality(Modality.WINDOW_MODAL);
+//					addStage.initOwner(btnList.getScene().getWindow());
+//					
+//					try {
+//						Parent cont = FXMLLoader.load(getClass().getResource("Root.fxml"));
+//						
+//						Scene scene = new Scene(cont);
+//						addStage.setScene(scene);
+//						addStage.show();
+//
+//					} catch (IOException e) {
+//						e.printStackTrace();
+//					}
+//			} else {
+//				return;
+//			}
+//			}
+//		});
 		
 		
-		
-		
-	}//initialize
-	
-	
+	}// initialize
+
 	public ObservableList<Book> getBookList() {
 		ObservableList<Book> list = FXCollections.observableArrayList();
 		String sql = "select id, title, writer, status, content, image from book";
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				Book board = new Book(rs.getInt("id"), rs.getString("title"), rs.getString("writer"), rs.getInt("status"), rs.getString("content"), rs.getString("image"));
+			while (rs.next()) {
+				Book board = new Book(rs.getInt("id"), rs.getString("title"), rs.getString("writer"),
+						rs.getInt("status"), rs.getString("content"), rs.getString("image"));
 				list.add(board);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return list;
-	} //getBookList
-	
-	
-	
+	} // getBookList
+
 	public void handleBtnListAction(ActionEvent ae) {
 		Stage addStage = new Stage(StageStyle.UTILITY);
 		addStage.initModality(Modality.WINDOW_MODAL);
 		addStage.initOwner(btnList.getScene().getWindow());
-		
+
 		try {
 			Parent parent = FXMLLoader.load(getClass().getResource("List.fxml"));
 			Scene scene = new Scene(parent);
-			
+
 			addStage.setScene(scene);
 			addStage.setResizable(false);
 			addStage.show();
-			
-			
+
 			ImageView imageView = (ImageView) parent.lookup("#imageView");
-			imageView.setImage(new Image("/images/" + book.getImage()+".jpg"));
-			
-			
+			imageView.setImage(new Image("/images/" + book.getImage() + ".jpg"));
+
 			Button listBorrow = (Button) parent.lookup("#listBorrow");
 			listBorrow.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent arg0) {
-					
-					
+
 					addStage.close();
 				}
 			});
-			
+
 			Button listCancel = (Button) parent.lookup("#listCancel");
 			listCancel.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
@@ -141,30 +169,22 @@ public class BookController implements Initializable {
 					addStage.close();
 				}
 			});
-			
-			
+
 			Label listTitle = (Label) parent.lookup("#listTitle");
 			listTitle.setText(book.getTitle());
-			
+
 			TextArea listContent = (TextArea) parent.lookup("#listContent");
 			listContent.setWrapText(true);
 			listContent.setText(book.getContent());
-			
-			
-		}catch (IOException e1) {
+
+		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 	}
-	
-	
-	
+
 	public void handleBtnCancelAction(ActionEvent e) {
 		Platform.exit();
 
 	}
-	
+
 }
-
-
-
-
